0e81852a778c0072e8bae1fe74846eb98acd75a4
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-17 18:23:17 -0400

    Adding in functionality for the post request

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const app = express();
 app.get('/', (req, res) => {
     res.send(`
     <div>
-        <form>
+        <form method="POST">
             <input name="email" placeholder="email"/>
             <input namee="password" placeholder="password"/>
             <input name="passwordConfirmation" placeholder="password confirmation"/>
             <button>Sign Up</button>
         </form>
     </div>`);
-});
+})
 
-// });
+//Listen to post
+app.post('/', (req, res) => {
+    res.send('Account created!!!')
+});
 //start listening for requests
 app.listen(3000, () => {
     console.log('Listening')
