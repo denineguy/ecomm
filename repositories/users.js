3dfda8b13a40e629ba3d7b045e77fae37994190e
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-17 22:03:30 -0400

    create, savind, setting random id and find record and deleting

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const crypto = require('crypto');
+const { REPL_MODE_SLOPPY } = require('repl');
 
 class UsersRepository {
     constructor(filename) {
@@ -21,14 +23,46 @@ class UsersRepository {
         }));
 
     };
+
+    async create(attrs) {
+        attrs.id = this.randomId();
+        const records = await this.getAll();
+        records.push(attrs)
+       
+        await this.writeAll(records)
+
+    }
+
+    async writeAll(records) {
+         //write the updated records array back to this.filename
+        await fs.promises.writeFile(
+            this.filename, 
+            JSON.stringify(records, null, 2)
+            );
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(record => record.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRecords); 
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-   const users = await repo.getAll();
+    await repo.delete('e3d9d28c');
 
-   console.log(users);
+    // console.log(user);
 };
 
 test();
