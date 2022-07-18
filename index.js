63f5273957f9d0381ccd0089c1c9f1981d97d62b
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-17 22:53:44 -0400

    signup validation logic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { is } = require('express/lib/request');
+const usersRepo = require('./repositories/users');
 
 //what kind of request it shoudl receive and what it shoud do
 const app = express();
@@ -21,9 +23,17 @@ app.get('/', (req, res) => {
 
 
 //Listen to post
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const {email, password, passwordConfirmation} = req.body
     //get access to email, password, and password confirmation
+    const existingUser = await usersRepo.getOneBy({email})
+    if (existingUser) {
+        res.send('Email in use');
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Passwords must match');
+    }
     
     res.send('Account created!!!')
 });
