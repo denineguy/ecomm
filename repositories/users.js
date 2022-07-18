97ca20e60c16270285b07719d168c3ea58e53809
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-17 21:29:50 -0400

    small refactor

--- repositories/users.js
+++ repositories/users.js
@@ -16,23 +16,19 @@ class UsersRepository {
     }
     async getAll() {
         //Open the file called this.filename
-        const contents = await fs.promises.readFile(this.filename, {
+        return JSON.parse(await fs.promises.readFile(this.filename, {
             encoding: 'utf8'
-        });
+        }));
 
-        //Read its contents
-        console.log(contents)
-
-        //parse the contents
-
-        //Return the parsed data
     };
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.getAll();
+   const users = await repo.getAll();
+
+   console.log(users);
 };
 
 test();
