b76fbb1b84e4e2c9e044dc8e2e5fee2e7ede419d
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-21 14:12:27 -0400

    adding some styling

--- index.js
+++ index.js
@@ -3,22 +3,16 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 
-const { rmSync } = require('fs');
-const users = require('./repositories/users');
-
-
-//what kind of request it shoudl receive and what it shoud do
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieSession({
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
     keys: ['lkasld235j']
-}));
+  })
+);
 app.use(authRouter);
 
-
-
-//start listening for requests
 app.listen(3000, () => {
-    console.log('Listening')
+  console.log('Listening');
 });
