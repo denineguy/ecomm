0250e635b0fe64ddc029fcb173190325971ac2a6
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-22 19:27:06 -0400

    Adding middlewares and validators

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
+
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
-const upload = multer({storage: multer.memoryStorage() });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -15,14 +16,17 @@ router.get('/admin/products/new', (req, res) => {
 });
 
 router.post(
-  '/admin/products/new', 
-  [requireTitle, requirePrice], 
-  upload.single('image'), 
-  (req, res) => {
-    const errors = validationResult(req);
+  '/admin/products/new',
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
+  async (req, res) => {
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
 
-    console.log(req.file);
     res.send('submitted');
-});
+  }
+);
 
 module.exports = router;
