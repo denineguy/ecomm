64973856617c916e9e3f9d1ad2b17576a6f2db3f
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-23 14:41:04 -0400

    Shopping cart boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const router = express.Router();
+//Receive a post request to add an item to a cart
+
+//Receive a GET request to show all items in cart
+
+//Recieve a post to delete an item
+
+
+
+module.exports = router;
