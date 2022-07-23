82bb0350feec3e5138ea82e9147daa95e3cabb15
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-23 19:59:27 -0400

    setting up remiving items from cart

--- routes/carts.js
+++ routes/carts.js
@@ -56,6 +56,8 @@ router.get('/cart', async (req, res) => {
 
 //Recieve a post to delete an item from a cart
 
-
+router.post('/cart/products/delete', async(req, res) => {
+    console.log(req.body.itemId);
+});
 
 module.exports = router;
